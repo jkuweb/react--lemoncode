6af8916c0b924abab2a79850672afceff21f31fd
Joseba <josebaquerejetaweb@gmail.com>
2024-02-07 08:26:15 +0100

    estilar la lista de miembros

--- src/member-list.tsx
+++ src/member-list.tsx
@@ -18,15 +18,22 @@ export const MemberList = () => {
   return (
     <>
       <h1>Member-list</h1>
-      <ul>
-        {members.map((member) => (
-          <li>
-            <img src={member.avatar_url} />
-            <span>{member.id}</span>
-            <span>{member.login}</span>
-          </li>
-        ))}
-      </ul>
+      <div className="member-list__container">
+        <div className="member-list__header">
+          <span>Avatar</span>
+          <span>ID</span>
+          <span>Login</span>
+        </div>
+        <ul className="member-list__list">
+          {members.map((member) => (
+            <li key={member.id} className="member-list__list-item">
+              <img src={member.avatar_url} />
+              <span>{member.id}</span>
+              <span>{member.login}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
